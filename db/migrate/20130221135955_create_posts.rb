33b2d1f977fbfab672f72b50c42e10c003c0b733
Brayden Cleary <brayden.cleary@gmail.com>
2013-02-21 14:52:44 -0800

    wrote models and simple validations

--- db/migrate/20130221135955_create_posts.rb
+++ db/migrate/20130221135955_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |p|
-      p.text :body
-      p.string :title
-      p.string :author
+      p.text :body, :null => false
+      p.string :title, :null => false
+      p.string :author, :null => false
       p.integer :like_count, :default => 0
       p.timestamps
     end
